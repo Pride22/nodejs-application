4d42c9367f464e6dfe40fa743b23269e7c68ef47
Pride22 <105609100+Pride22@users.noreply.github.com>
2023-07-01 13:35:46 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.88.239:9000/',
+  serverUrl: 'http://172.31.92.43:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
